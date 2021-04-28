0fc4133289b6a568ce97c988b4e5eb75b66fe01d
Jesse Braham <jesse@beta7.io>
2021-04-27 21:16:37 -0700

    Fairly significant rework of event handling and window layout

--- layout.rkt
+++ layout.rkt
@@ -10,10 +10,10 @@
 
 
 (define (shift-left xs)
-  (when (> (length xs) 1) (cons (last xs) (drop-right xs 1))))
+  (when (> (length xs) 1) (append (rest xs) (list (first xs)))))
 
 (define (shift-right xs)
-  (when (> (length xs) 1) (append (rest xs) (list (first xs)))))
+  (when (> (length xs) 1) (cons (last xs) (drop-right xs 1))))
 
 
 (struct rect (x y w h) #:transparent)
@@ -25,13 +25,13 @@
 
     (field [ws '()])
 
-    (define/public (get-ws) ws)
+    (define/public (get-wins) ws)
 
     (define/public (push-win   w) (set! ws (append ws (list w))))
     (define/public (remove-win w) (set! ws (remove w ws)))
 
-    (define/public (rotate-left)  (set! ws (shift-left  ws)))
-    (define/public (rotate-right) (set! ws (shift-right ws)))))
+    (define/public (rotate-left)  (set! ws (shift-right ws)))
+    (define/public (rotate-right) (set! ws (shift-left  ws)))))
 
 
 (define layout-engine%
@@ -40,86 +40,134 @@
 
     (init-field xdisplay screen display-w display-h)
 
-    (field [layout-t   '(horizontal vertical fullscreen)]
+    (field [layout-t  '(horizontal vertical fullscreen)]
            [root-rect  (rect 0 0 display-w display-h)]
            [current-ws (new workspace%)]
            [wss        (hash 0 current-ws)])
 
-    (define/public (switch-workspace ws-id)
-      (define new-ws (hash-ref wss ws-id
-                               (λ ()
-                                 (let ([new-ws (new workspace%)])
-                                   (set! wss (hash-set wss ws-id new-ws))
-                                   new-ws))))
-      (when (not (eq? current-ws new-ws))
-        (hide-all (send current-ws get-ws))
-        (set! current-ws new-ws)
-        (layout)))
+    ;; Eventually these values will be loaded from a configuration file or
+    ;; something, but until then we'll just define them here for convenience.
+    (define BORDER   4)
+    (define PADDING 20)
 
-    (define/public (prev-layout)
-      (set! layout-t (shift-left layout-t))
-      (layout))
-    (define/public (next-layout)
-      (set! layout-t (shift-right layout-t))
-      (layout))
+    ;; ------------------------------------------------------------------------
+    ;; Public Methods
 
     (define/public (push-win w)
       (send current-ws push-win w)
+      (XSetWindowBorderWidth xdisplay w BORDER)
       (layout))
+
     (define/public (remove-win w)
       (send current-ws remove-win w)
       (layout))
 
-    (define/public (rotate-left)
-      (send current-ws rotate-left)
+    (define/public (switch-workspace ws-id)
+      (let ([new-ws (wss-ref ws-id)])
+        (when (not (eq? current-ws new-ws))
+          (hide-all (send current-ws get-wins))
+          (set! current-ws new-ws)
+          (layout))))
+
+    (define/public (move-to-workspace ws-id)
+      (let* ([new-ws (wss-ref ws-id)]
+             [w      (send current-ws get-focus)])
+        (unless (or (eq? current-ws new-ws) (null? w))
+          (hide       w)
+          (remove-win w) ; invokes `layout`
+          (send new-ws push-win w))))
+
+    (define/public (prev-layout)
+      (set! layout-t (shift-right layout-t))
       (layout))
-    (define/public (rotate-right)
+
+    (define/public (next-layout)
+      (set! layout-t (shift-left layout-t))
+      (layout))
+
+    (define/public (rotate-ccw)
       (send current-ws rotate-right)
       (layout))
 
-    ;; Private
-    (define (layout)
-      (match (send current-ws get-ws)
-        [(list)   (void)]
-        [(list w) (fullscreen w)]
-        [(list-rest m ws)
+    (define/public (rotate-cw)
+      (send current-ws rotate-left)
+      (layout))
+
+    ;; ------------------------------------------------------------------------
+    ;; Private Methods
+
+    (define/private (wss-ref ws-id)
+      (hash-ref wss ws-id (λ ()
+                            (let ([new-ws (new workspace%)])
+                              (set! wss (hash-set wss ws-id new-ws))
+                              new-ws))))
+
+    (define/private (layout)
+      (match (send current-ws get-wins)
+        [(list)    (void)]
+        [(list  w) (fullscreen w)]
+        [(list* m ws)
          (case (first layout-t)
-           [(fullscreen) (fullscreen m)
-                         (hide-all   ws)]
-           [else
-            (define-values (s1 s2)
-              (if (eq? (first layout-t) 'horizontal)
-                  (values split-horizontal split-vertical)
-                  (values split-vertical   split-horizontal)))
-            (match-define (list r1 r2) (s1 root-rect 2))
-            (size m r1)
-            (for ([w ws]
-                  [r (s2 r2 (length ws))])
-              (size w r))])]))
+           [(horizontal) (tile-horizontal m ws)]
+           [(vertical)   (tile-vertical m ws)]
+           [(fullscreen) (hide-all   ws)
+                         (fullscreen m)])]))
 
-    (define (size-and-show-window win r)
-      (match-define (rect x y w h) r)
-      (XMoveResizeWindow xdisplay win x y w h)
-      (XMapWindow        xdisplay win))
+    (define/private (tile-horizontal m ws)
+      (match-let* ([(list r1 r2)       (split-rect root-rect 2 'horizontal)]
+                   [(rect x1 y1 w1 h1) r1]
+                   [(rect x2 y2 w2 h2) r2])
+        (let* ([hp (/ PADDING 2)]
+               [vp (/ PADDING (length ws))])
+          (size m (rect x1 y1 (+ w1 hp) h1))
+          (for ([win ws]
+                [i   (range (length ws))]
+                [r   (split-rect r2 (length ws) 'vertical)])
+            (match-let ([(rect x y w h) r])
+              (size win (rect (- x hp)
+                              (- y (* i vp))
+                              (+ w hp)
+                              (+ h (if (eq? (length ws) 1) 0 vp)))))))))
 
-    (define (fullscreen w)   (size-and-show-window w root-rect))
-    (define (size       w r) (size-and-show-window w r))
-    (define (hide       w)   (XUnmapWindow xdisplay w))
-    (define (hide-all   ws)  (for ([w ws]) (hide w)))
+    (define/private (tile-vertical m ws)
+      (match-let* ([(list r1 r2)       (split-rect root-rect 2 'vertical)]
+                   [(rect x1 y1 w1 h1) r1]
+                   [(rect x2 y2 w2 h2) r2])
+        (let* ([hp (/ PADDING 2)]
+               [vp (/ PADDING (length ws))])
+          (size m (rect x1 y1 w1 (+ h1 hp)))
+          (for ([win ws]
+                [i   (range (length ws))]
+                [r   (split-rect r2 (length ws) 'horizontal)])
+            (match-let ([(rect x y w h) r])
+              (size win (rect (- x (* i vp))
+                              (- y hp)
+                              (+ w (if (eq? (length ws) 1) 0 vp))
+                              (+ h hp))))))))
 
-    (define (split-horizontal r c)
-      (match-define  (rect x y w h) r)
-      (define-values (q rem) (quotient/remainder w c))
-      (for/list ([i c])
-        (let ([x (+ x (* i q) (if (< i rem) i 0))]
-              [w (+ q (if (< i rem) 1 0))])
-          (rect x y w h))))
+    (define/private (split-rect r n axis)
+      (match-let* ([horizontal?    (eq? axis 'horizontal)]
+                   [(rect x y w h) r])
+        (let-values ([(q rem) (quotient/remainder (if horizontal? w h) n)])
+          (for/list ([i n])
+            (let-values ([(wx ww) (axis-position-dimension x q rem i)]
+                         [(wy wh) (axis-position-dimension y q rem i)])
+              (if horizontal? (rect wx y ww h) (rect x wy w wh)))))))
 
-    (define (split-vertical r c)
-      (match-define  (rect x y w h) r)
-      (define-values (q rem) (quotient/remainder h c))
-      (for/list ([i c])
-        (let ([y (+ y (* i q) (if (< i rem) i 0))]
-              [h (+ q (if (< i rem) 1 0))])
-          (rect x y w h))))))
+    (define/private (axis-position-dimension p q rem i)
+      (let* ([ii (if (< i rem) i 0)]
+             [a  (+ p (* i q) ii)]
+             [b  (+ q ii)])
+        (values a b)))
 
+    (define/private (hide       w)  (XUnmapWindow xdisplay w))
+    (define/private (hide-all   ws) (for ([w ws]) (hide w)))
+    (define/private (fullscreen w)  (size w root-rect))
+
+    (define/private (size win r)
+      (match-let* ([bb (* 2 BORDER)]
+                   [pp (* 2 PADDING)]
+                   [(rect x y w h) r])
+        (XMoveResizeWindow
+         xdisplay win (+ x PADDING) (+ y PADDING) (- w bb pp) (- h bb pp))
+        (XMapWindow xdisplay win)))))
--- main.rkt
+++ main.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 
-(require ffi/unsafe/port
+(require (only-in ffi/unsafe/port
+                  [unsafe-file-descriptor->port unsafe-fd->port])
          racket/class
          x11/keysymdef
          x11/x11
@@ -14,61 +15,66 @@
   (log  message)
   (exit 1))
 
-(define (config-root-window xdisplay root display-w display-h)
-  (XClearArea   xdisplay root 0 0 display-w display-h #f)
-  (XSelectInput xdisplay root '(EnterWindowMask
-                                LeaveWindowMask
-                                KeyPressMask
-                                SubstructureNotifyMask
-                                SubstructureRedirectMask)))
 
-(define (grab-key xdisplay root key)
-  (let ([k (XKeysymToKeycode xdisplay key)])
-    (XGrabKey xdisplay k 'ControlMask root #t 'GrabModeAsync 'GrabModeAsync)))
+(define (config-root-window xdisplay screen root display-w display-h)
+  (XClearArea   xdisplay root 0 0 display-w display-h #f)
+  (XSelectInput xdisplay root '(KeyPressMask
+                                SubstructureNotifyMask
+                                SubstructureRedirectMask))
+  ;; Set the background color to something vibrant while we're developing.
+  ;; This will eventually be user-configurable I'd imagine.
+  (let* ([black   (BlackPixel xdisplay screen)]
+         [crimson (AllocNamedColor xdisplay screen "crimson" black)])
+    (XSetWindowBackground xdisplay root crimson)
+    (XClearWindow         xdisplay root)))
+
+(define (grab-keys xdisplay root mask keys)
+  (for ([key keys])
+    (let ([k (XKeysymToKeycode xdisplay key)])
+      (XGrabKey xdisplay k mask root #t 'GrabModeAsync 'GrabModeAsync))))
+
+
+(define (xk-key->ws-id key) (if (eq? key 48) 9 (- key 49)))
+
+(define (handle-keypress-event xdisplay engine event)
+  (let ([xk-key (XKeycodeToKeysym xdisplay (XKeyEvent-keycode event) 0)])
+    (case (XKeyEvent-state event)
+      ;; META + SHIFT
+      [((Mod1Mask ShiftMask))
+       (case xk-key
+         ;; SPACE
+         [(32)
+          (send engine prev-layout)]
+         ;; 0 - 9
+         [(48 49 50 51 52 53 54 55 56 57)
+          (send engine move-to-workspace (xk-key->ws-id xk-key))])]
+      ;; META
+      [((Mod1Mask))
+       (case xk-key
+         ;; SPACE
+         [(32)
+          (send engine next-layout)]
+         ;; 0 - 9
+         [(48 49 50 51 52 53 54 55 56 57)
+          (send engine switch-workspace (xk-key->ws-id xk-key))]
+         ;; h
+         [(104)
+          (send engine rotate-ccw)]
+         ;; l
+         [(108)
+          (send engine rotate-cw)])])))
 
 (define (handle-x11-event xdisplay engine)
-  (define event (XNextEvent* xdisplay))
-  (case (XEvent-type event)
-    ;; Keyboard
-    [(KeyPress)
-     ; CTRL
-     (when (equal? '(ControlMask) (XKeyEvent-state event))
-       (define xk-key (XKeycodeToKeysym xdisplay (XKeyEvent-keycode event) 0))
-       (case xk-key
-         ; F1 - F12
-         [(65470 65471 65472 65473 65474 65475 65476 65477
-                 65478 65479 65480 65481)
-          (send engine switch-workspace (- xk-key 65470))] ; zero-indexed
-         ; space
-         [(32)  (send engine next-layout)]
-         ; j
-         [(106) (send engine rotate-left)]
-         ; k
-         [(107) (send engine rotate-right)]
-         [else
-          (log (format "key-event ~a ~a ~a"
-                       (XKeyEvent-state   event)
-                       (XKeyEvent-keycode event)
-                       xk-key))]))]
-    [(KeyRelease) (void)]
-    ;; Window Crossing
-    [(EnterNotify) (void)]
-    [(LeaveNotify) (void)]
-    ;; Structure Control
-    [(ConfigureRequest) (void)]
-    [(MapRequest)
-     (send engine push-win (XMapRequestEvent-window event))]
-    [(ConfigureNotify) (void)]
-    [(CreateNotify)    (void)]
-    [(DestroyNotify)
-     (send engine remove-win (XDestroyWindowEvent-window event))]
-    ;; Window State Notification
-    [(MapNotify)     (void)]
-    [(MappingNotify) (void)]
-    [(UnmapNotify)   (void)]
-    ;; Unhandled events
-    (else
-     (log (format "event: ~a" (XEvent-type event))))))
+  (let ([event (XNextEvent* xdisplay)])
+    (case (XEvent-type event)
+      ;; Keyboard
+      [(KeyPress)
+       (handle-keypress-event xdisplay engine event)]
+      ;; Structure Control
+      [(MapRequest)
+       (send engine push-win (XMapRequestEvent-window event))]
+      [(DestroyNotify)
+       (send engine remove-win (XDestroyWindowEvent-window event))])))
 
 
 (define (tanuki-wm)
@@ -76,36 +82,45 @@
   ;; connection cannot be established we cannot continue any further.
   (define xdisplay (XOpenDisplay #f))
   (unless xdisplay (panic! "unable to open display"))
+  (log (format "connected to x server display ~a" (XDisplayString xdisplay)))
 
-  ;; Since we only support a single display at this point in time, we use the
-  ;; `DefaultScreen` macro to retrieve the screen number.
+  ;; Since we only support a single display at this point in time, we use
+  ;; `DefaultScreen` to retrieve the screen number.
   (define screen    (DefaultScreen xdisplay))
   (define display-w (DisplayWidth  xdisplay screen))
   (define display-h (DisplayHeight xdisplay screen))
 
-  ;; Initialize and configure the root window.
+  ;; Initialize and configure the root window. This applies the relevant event
+  ;; masks and sets the background.
   (define root-window (RootWindow xdisplay screen))
-  (config-root-window xdisplay root-window display-w display-h)
+  (config-root-window xdisplay screen root-window display-w display-h)
   (log "root window has been configured")
 
-  ;; Enable 'KeyPress' events for all relevant key combinations.
-  ;; TODO: one day keyboard shortcuts should be configurable...
-  (for ([key (list XK-F1 XK-F2 XK-F3 XK-F4 XK-F5 XK-F6 XK-F7 XK-F8
-                   XK-F9 XK-F10 XK-F11 XK-F12 XK-space XK-j XK-k)])
-    (grab-key xdisplay root-window key))
+  ;; Enable 'KeyPress' events for all relevant key combinations. We must be
+  ;; careful to only mask the key combinations we want to capture, otherwise
+  ;; we may prevent certain key combinations in other applications.
+  (define alpha-keys (list XK-h XK-l))
+  (define num-keys   (list XK-0 XK-1 XK-2 XK-3 XK-4 XK-5 XK-6 XK-7 XK-8 XK-9))
+  (define other-keys (list XK-space))
+
+  (define mod-shift-keys (append            num-keys other-keys))
+  (define mod-keys       (append alpha-keys num-keys other-keys))
+
+  (grab-keys xdisplay root-window '(Mod1Mask ShiftMask) mod-shift-keys)
+  (grab-keys xdisplay root-window '(Mod1Mask)           mod-keys)
+  (log "required keys have been grabbed")
+
+  ;; Open the X11 port in 'read' mode.
+  (define x11-port
+    (unsafe-fd->port (XConnectionNumber xdisplay) "x11-port" '(read)))
+  (log "x11 port has been opened for reading")
 
   ;; Initialize the tiling window layout engine. Handles sizing and placement
   ;; of windows.
   (define engine
     (make-object layout-engine% xdisplay screen display-w display-h))
 
-  ;; Open the X11 port in 'read' mode.
-  (define x11-port
-    (unsafe-file-descriptor->port
-     (XConnectionNumber xdisplay) "x11-port" '(read)))
-  (log "x11 port has been opened for reading")
-
-  ;; Run the event loop until the application terminates.
+  ;; Run the event loop until the application terminates (or forever!).
   (let event-loop ()
     (sync/enable-break
      (handle-evt x11-port
@@ -116,10 +131,14 @@
                        (x11-port-loop)))))
      (handle-evt (current-input-port)
                  (λ (e) (log (format "input ~a ~a" e (read-line e))))))
-    (event-loop)))
+    (event-loop))
+
+  ;; Close the connection to the X server if/when the event loops breaks.
+  (log "tanuki-wm is shutting down")
+  XCloseDisplay(xdisplay))
 
 
 (module+ main
-  (log "it's tanuki time!")
+  (log "tanuki-wm is starting up")
   (tanuki-wm))
 
